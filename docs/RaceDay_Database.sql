a0d1e8c36048d907cd4f66d738b7b2a2e39a7fb0
Adelia Antonio <164026739+ST10437150@users.noreply.github.com>
2026-09-04 21:42:24 +0200

    Update header formatting in RaceDay_Database.sql

--- docs/RaceDay_Database.sql
+++ docs/RaceDay_Database.sql
@@ -1,4 +1,4 @@
--- ============================================
+--- ============================================
 -- RaceDay Database Script
 -- Part 1 - System Planning and Database
 -- Run this script in SQL Server Management Studio (SSMS)
